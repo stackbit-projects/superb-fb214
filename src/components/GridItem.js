eab199bbc91c082a1872bcaef7dbe3515dc1d353
Stackbit Code Editor <projects@stackbit.com>
2021-05-05 14:51:17 +0000

    GridItem.js: changed

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -26,7 +26,7 @@ export default class GridItem extends React.Component {
         let is_card_has_padding = false;
         let is_card_no_padding = false;
         let is_card_no_img_no_padding = false;
-        let bg_color = _.get(item, 'background_color', null) || 'none'; //NEW
+        let bg_color = _.get(section, 'background_color', null) || 'none'; //NEW
         if (_.get(section, 'enable_cards', null)) {
              is_card = true;
         }
